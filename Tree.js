f44d7a14567af2a81c4b4ef1154ef10021233212
Yashaswi2006 <71638411+Yashaswi2006@users.noreply.github.com>
2020-11-04 21:11:17 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,15 @@
+class Tree {
+    constructor(x,y,width,height){
+      var options= {
+isstatic:false,
+restitution : 0,
+friction: 1,
+density:1.2
+      }
+    }
+  
+    display() {
+   
+    }
+  }
+  
